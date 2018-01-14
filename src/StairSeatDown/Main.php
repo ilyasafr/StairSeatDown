282e5cb4bca7d3a33e6c5156732d2ef986af2791
gamesukimanIRS <gamesukiman@gmail.com>
2018-01-14 17:37:32 +0900

    Update Main.php

--- src/StairSeatDown/Main.php
+++ src/StairSeatDown/Main.php
@@ -25,7 +25,7 @@ class Main extends PluginBase implements Listener {
 		$version = "1.0.0";
     		$this->getServer()->getPluginManager()->registerEvents($this, $this);
     		$this->getlogger()->info($PluginName."Version:".$version."を読み込みました。作者:gamesukimanIRS, maru");
-    		$this->getlogger()->warning("製作者偽りと二次配布、改造、改造配布はおやめ下さい。");
+    		$this->getlogger()->warning("このプラグインはLGPLライセンスにより配布されています。");
 		$this->getLogger()->notice("このプラグインのコードの元はPmChairより引用しています。")
     		$this->getlogger()->info("このプラグインを使用する際はどこかにプラグイン名「".$PluginName."」と作者名「gamesukimanIRS, maru」を記載する事を推奨します。");
 		if(!file_exists($this->getDataFolder())){ 
